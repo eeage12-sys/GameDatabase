cdc2466da745d9948d3dc6b02d3b2368a52f3160
eeage12-sys <eeage12@gmail.com>
2026-09-21 17:36:04 +0900

    2026 09 21

---
+++ GameDatabaseLab/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace GameDatabaseLab
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (SqliteConnection connection =
+                   new SqliteConnection("Data Source=GameShop.db"))
+            {
+                connection.Open();
+
+                using (SqliteCommand pragma = connection.CreateCommand())
+                {
+                    pragma.CommandText = "PRAGMA foreign_keys = ON;";
+                    pragma.ExecuteNonQuery();
+                }
+
+                try
+                {
+                    using (SqliteCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = @"
+INSERT INTO Inventory (PlayerId, ItemId, Quantity)
+VALUES ($playerId, $itemId, $quantity);";
+
+                        command.Parameters.AddWithValue("$playerId", 1);
+                        command.Parameters.AddWithValue("$itemId", 999);
+                        command.Parameters.AddWithValue("$quantity", 1);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (SqliteException e)
+                {
+                    Console.WriteLine("등록 거부됨");
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
